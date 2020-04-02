b4d48ee8a52679bbe2a160581743d64973a44e1a
vincent raveneau <vincent.raveneau@epitech.eu>
2020-04-02 10:06:05 +0200

    feat: add index.js(Roles db)

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: "mariadb",
+    storage: "db.mariadb"
+});
+
+
+const Roles = sequelize.define('Roles', {
+    role_id: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    role_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    admin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false
+    },
+    recommend: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    unique: false,
+    },
+    write_article: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        unique: false,
+    },
+    add_member: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        unique: false,
+    },
+    create_project: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        unique: false
+    }
+});

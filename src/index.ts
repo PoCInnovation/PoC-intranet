eb2db8853e541f0695385ebc417f3e215d5a1a25
Tom Chauveau <tom.chauveau@epitech.eu>
2020-04-24 16:35:04 +0200

    refactor(src/models) split functions to be more clear fix(src/models): Fix some error detected by the test test(src/test): Add basic test on DB to verify if functions are correct

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,9 @@
-import {PrismaClient, User, Role, Project} from "@prisma/client"
-import { findAllUser, findUserIdByMail, createUser, deleteUser, linkUserToRole } from "./models/user";
-import {Perms, findAllRoles, createRoles, findRoleByName} from "./models/roles";
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-/*
-    ? Access to an element by a relation : table?.relation[index].row
- */
-
-// ? Crud for user : OK
-// Todo Crud for roles and link to user : In progress
-// Todo Crud for project and link to user
 async function main() {
-//    await createUser({mail: "mdrsdfqsdfg", token: "mdrqsdfghgfds"});
-    let perm: Perms = {
-        admin: false,
-        create_pro: false,
-        write: true,
-        recommend: true,
-        add_member: false,
-        add_role: true
-    };
-//    await createRoles(perm, "test2");
-//    let users: User[] = await findAllUser();
-//    users.forEach(obj => console.log(obj));
-//    return;
-  // ? test link user à role
-    let role: Role | null = await findRoleByName("test2")
-    console.log(role?.name)
-    let user: User | null = await findUserIdByMail("mdr");
-    console.log(user?.id)
-    if (role?.name != null && user?.id != null)
-        console.log(await linkUserToRole(role?.name, user?.id))
- //   let result = await findUserIdByMail("mdr")
- //   console.log(result)
- //   console.log(await findAllUser())
-    let all = await findRoleByName("test2");
-    console.dir(all?.users[0].mail);
- //   console.log(await findAllRoles())
+    console.log("Index: run test to see what append");
 }
 
 main()
---
+++ src/tests/test_user.ts
@@ -0,0 +1,73 @@
+import { User } from "@prisma/client";
+import { findAllUser, findUserIdByMail, createUser, deleteUser, UserInfo } from "../models/user";
+
+// ? --------------------------- TEST --------------------------- ? //
+async function testCreation(newUser: UserInfo) {
+    let result: User = await createUser(newUser)
+
+    if (newUser.mail != result.mail || newUser.token != result.token) {
+        console.error("Error on Creation");
+        return 1;
+    }
+    return 0;
+}
+
+async function testFindUserIdByMail(mail: string)
+{
+    let result = await findUserIdByMail(mail);
+
+    if (result == null || mail != result.mail) {
+        console.error("Error on finUserIdByMail");
+        return 1;
+    }
+    return 0;
+}
+
+async function testFindAllUser(newUser: UserInfo, newUser2: UserInfo) {
+    let result = await findAllUser();
+
+    if (newUser.mail != result[0].mail || newUser.token != result[0].token) {
+        console.error("Error on findAllUser");
+        return 1;
+    }
+    if (newUser2.mail != result[1].mail || newUser2.token != result[1].token) {
+        console.error("Error on findAllUser");
+        return 1;
+    }
+    return 0;
+}
+// ? ----------------------------------------------------------- ? //
+
+// ? Clean my Db
+async function clearDB(newUser: UserInfo, newUser2: UserInfo) {
+    let user1 = await findUserIdByMail(newUser.mail);
+    let user2 = await findUserIdByMail(newUser2.mail);
+
+    if (user1 != null && user1.id != null && user2 != null && user2.id != null) {
+        await deleteUser(user1?.id);
+        await deleteUser(user2.id);
+        return 0;
+    }
+    else
+        console.error("Error on cleaning of the DB");
+        return 1;
+}
+
+async function testUserModel() {
+    let score: number = 0;
+    let newUser: UserInfo = { mail: "test4", token: "token4" };
+    let newUser2: UserInfo = { mail: "test5", token: "token5"};
+
+    console.log("---- TEST USER ----")
+    score += await testCreation(newUser);
+    score += await testCreation(newUser2);
+    score += await testFindUserIdByMail(newUser.mail);
+    score += await testFindAllUser(newUser, newUser2);
+    score += await clearDB(newUser, newUser2);
+
+    console.log(score == 0 ? "\nEverything worked good" : "\nError find");
+    console.log("---- DB ----")
+    console.log(await findAllUser());
+}
+
+export { testUserModel };

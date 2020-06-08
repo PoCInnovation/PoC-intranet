7e3c29ff875cb792f102bef797c61639bb152f68
vincentraveneau <vincentraveneau@WIN-U5P6CM8688T.home>
2020-06-08 07:09:58 +0200

    feat : Add started front

--- front/src/components/home_app.js
+++ front/src/components/home_app.js
@@ -1,10 +1,36 @@
 import React from "react";
 import './home_app.css';
+import logo from '../logoPoC_white.svg'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  
 
-function homePage() {
+function HomePage() {
     return(
-        <p>Hello world</p>
+      <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <div className="App-Welcome">Bienvenue, User !</div>
+              <div className="Rectangle-text">
+              <div className="App-ProjectName">Project Name </div>
+              <div className="App-Rectangle-Task">Task</div>
+              </div>
+            </header>
+      </div>
     )
 }
 
-export default homePage;
+export default function App() {
+    return (
+        <Router>
+          <Switch>
+            <Route path="/" component={ HomePage } />
+            <Route path="/home" component= { HomePage } />
+          </Switch>
+        </Router>
+      );
+}
